b0159d7cad05e6cd4cdbdc51a9d3870e4c7a18c8
rafael lisboa <rafaelzlisboa@gmail.com>
2015-05-25 12:22:34 -0300

    emacs: dont run commands dependent on packages if package is not installed

--- emacs.el
+++ emacs.el
@@ -1,23 +1,27 @@
+;; recommended packages:
+;; - expand-region
+;; - zenburn-theme
+;; and:
+;; - fortune command
+
 (package-initialize)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq inhibit-splash-screen t)
 (setq-default indent-tabs-mode nil)
-(load-theme 'zenburn t)
+;; (load-theme 'zenburn t)
 
 
 (defun shell-command-to-string-no-newline(command)
   (substring (shell-command-to-string command) 0 -1))
 
-(setq random-word-oneliner "python -c \"import os, random; wordsfile = '/usr/share/dict/words'; words = open(wordsfile, 'r')\; wordsfile_size = os.stat(wordsfile)[6]; words.seek((random.randint(0, wordsfile_size - 1)) % wordsfile_size); words.readline(); print words.readline(),\"")
-(defun startup-echo-area-message() 
-  (message (concat "Before you go, take a random word: " (shell-command-to-string-no-newline random-word-oneliner))))
-
-(setq initial-scratch-message
-  (concat (replace-regexp-in-string "^" "; " (shell-command-to-string-no-newline "/usr/local/bin/fortune wisdom")) "\n\n"))
-
-
+(when (executable-find fortune)
+  (setq initial-scratch-message
+    (concat (replace-regexp-in-string "^" ";; "
+                                      (shell-command-to-string-no-newline
+                                        "/usr/local/bin/fortune wisdom"))
+            "\n\n")))
 
 (icomplete-mode t)
 (ido-mode t)
@@ -34,13 +38,13 @@
   (eldoc-mode t))
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
-(require 'expand-region)
-(global-set-key (kbd "C-@") 'er/expand-region)
+
+(when (require 'expand-region nil 'noerror)
+  (global-set-key (kbd "C-@") 'er/expand-region))
 
 
 (custom-set-variables
   '(speedbar-show-unknown-files t)
   '(speedbar-smart-directory-expand-flag nil)
   '(speedbar-update-flag nil)
-  '(sr-speedbar-auto-refresh nil)
-)
+  '(sr-speedbar-auto-refresh nil))
