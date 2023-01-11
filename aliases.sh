f49f2eff7824671aa10df0ddbf3fd701e3ba0519
Rafael Lisboa <rafael.lisboa@rafael.lisboa>
2023-01-11 14:24:17 +0100

    remove unneeded and outdated aliases

--- aliases.sh
+++ aliases.sh
@@ -1,16 +1,12 @@
 alias ls="ls -Gh"
 alias ll="ls -la"
 alias gs="git status"
-alias gsr="git svn rebase"
 alias e='open -a Emacs "$@"'
 alias p="cd ~/Projects"
 alias d="cd ~/Desktop"
 alias tree="tree -C"
 alias f="find . -name"
 alias emacs="emacs -nw"
-
-alias iphone="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
-alias starbound="open '/Users/rafael/Library/Application Support/Steam/SteamApps/common/Starbound/Starbound.app'"
 
 alias slackscale="mogrify -resize 128x128 -unsharp 2x1.4+0.5+0 -quality 100 -verbose"
 
